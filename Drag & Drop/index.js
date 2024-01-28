d590dc51266f0c550558b49383a84ec7409d4edb
sabbirsun <sabbirsun1920@gmail.com>
2024-01-28 22:21:37 +0600

    drag and drop

--- Drag & Drop/index.js
+++ Drag & Drop/index.js
@@ -11,6 +11,7 @@ button.onclick = () => {
   input.addEventListener("change", function(){
     myFile = this.files[0];
     DragArea.classList.add("active");
+    ShowMe();
   })
 
   DragArea.addEventListener("dragover", (e)=>{
@@ -18,3 +19,34 @@ button.onclick = () => {
     DragArea.classList.add("active");
     DragText.textContent = "Relase to Upload File";
   })
+
+  DragArea.addEventListener("dragleave", ()=>{
+    DragArea.classList.remove("active");
+    DragText.textContent = "Drag & Drop";
+  })
+
+  DragArea.addEventListener("drop", (e) => {
+    e.preventDefault();
+    myFile = e.dataTransfer.files[0]; // Corrected typo here
+    ShowMe();
+})
+
+
+  function ShowMe(){
+    let fileType = myFile.type;
+    let valiEx = ["image/jpeg", "image/jpg", "image/png"];
+    if(valiEx.includes(fileType)){
+      let fileReader = new FileReader();
+      fileReader.onload = ()=>{
+        let imgURL = fileReader.result;
+        let img = `<img src="${imgURL}" alt="">`;
+        DragArea.innerHTML = img;
+      }
+      fileReader.readAsDataURL(myFile);
+    }else{
+      alert("This File is Not Valid");
+      DragArea.classList.remove("active");
+      DragArea.textContent = "Drag & Drop";
+    }
+  }
+
